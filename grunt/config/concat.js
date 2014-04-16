861a4e9a1f4d8bd7f7b919b22fa6e55148f6e37d
Rich Harris <richard.a.harris@gmail.com>
2014-04-16 10:29:32 -0400

    grunt stuff

---
+++ grunt/config/concat.js
@@ -0,0 +1,9 @@
+module.exports = {
+	bundle: {
+		src: 'src/ractive-transitions-fly.js',
+		dest: 'tmp/ractive-transitions-fly.js'
+	},
+	options: {
+		process: true
+	}
+};
---
+++ grunt/config/uglify.js
@@ -0,0 +1,6 @@
+module.exports = {
+	bundle: {
+		src: 'tmp/ractive-transitions-fly.js',
+		dest: 'tmp/ractive-transitions-fly.min.js'
+	}
+};
